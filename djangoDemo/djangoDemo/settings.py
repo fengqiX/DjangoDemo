3ed1d06f6d45a5df03501f2268ac381112e9002e
fxiao <fengqi.xiao.sd@gmail.com>
2019-12-30 15:27:11 -0700

    apply MVC pattern use reverse engine to create DB from models

--- djangoDemo/djangoDemo/settings.py
+++ djangoDemo/djangoDemo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_demo',  #  register new app
+    'hrs'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'djangoDemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'djangoDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodemo',
+        'HOST':'127.0.0.1',
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD':'password',
     }
 }
 
--- djangoDemo/djangoDemo/urls.py
+++ djangoDemo/djangoDemo/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from app_demo import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^hello$/', views.hello), # use url to find the def in views
+    path('hrs/',include('hrs.urls'))  # use include to get all the path in one time
 ]
