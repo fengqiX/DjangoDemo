1931bf7b0e06454753cb74e9d19d598106177508
fxiao <fengqi.xiao.sd@gmail.com>
2020-01-06 10:34:08 -0700

    modify Models and Templates

--- djangoDemo/polls/models.py
+++ djangoDemo/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime,timedelta
 # Create your models here.
 
 class Questions(models.Model):
@@ -9,7 +10,7 @@ class Questions(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pud_date >= timezone.now() - timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Questions, on_delete=models.CASCADE)
