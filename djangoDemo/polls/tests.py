2c203b3d1fb0d766693f50d855b7dfaee92da383
fxiao <fengqi.xiao.sd@gmail.com>
2020-01-06 10:35:41 -0700

    test case example * 'import datetime' used as 'datetime.timedelta()' which is not working. has to be 'from datetime import timedelta'

--- djangoDemo/polls/tests.py
+++ djangoDemo/polls/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
 
+from django.test import TestCase
+from django.utils import timezone
+from .models import Questions
+from datetime import datetime,timedelta
 # Create your tests here.
+
+
+class QuestionsModelTests(TestCase):
+
+    def test_was_pub_recently_with_future_question(self):
+        time = timezone.now()+timedelta(days=30)
+        future_question = Questions(pud_date = time)
+        self.assertIs(future_question.was_published_recently(),False)
